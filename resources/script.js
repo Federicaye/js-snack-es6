e984855cb31e2f9699ab73d06e13f3451c710060
Federicaye <gallofederica58@gmail.com>
2024-03-25 15:31:50 +0100

    add score and foul

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,4 @@
-let guest = [
+/* let guest = [
     {
         nameGuest: "Brad Pitt",
         id: 1
@@ -66,7 +66,7 @@ guest.forEach((guest, i) => {
 102 Piero della Francesca   50
 120 Francesca da Polenta    84 */
 
-const student =[
+/* const student =[
     {
         id:213,
         nameStudent: 'Marco della Rovere',
@@ -120,4 +120,77 @@ const gradesBest = student.filter((student) => {
 const gradesId = gradesBest.filter((student) => {
     if(student.id>120){return true}
 });
-console.log(gradesBest, gradesId);
+console.log(gradesBest, gradesId); */
+
+const bikes = [
+    {
+        nameBike: 'red',
+        weight: 10
+    },
+    {
+        nameBike: 'blue',
+        weight: 13
+    },
+    {
+        nameBike: 'black',
+        weight: 4
+    },
+    {
+        nameBike: 'orange',
+        weight: 10
+    },
+    {
+        nameBike: 'grey',
+        weight: 13
+    },
+    {
+        nameBike: 'white',
+        weight: 4
+    }
+]
+
+let bikeLight = bikes[0];
+for (let bike of bikes) { 
+    if (bike.weight < bikeLight.weight){
+        bikeLight = bike;
+    }
+
+}
+
+console.log(bikeLight);
+
+
+const teamList = [
+    {
+        namet: 'red',
+        score: 0,
+        foul: 0,
+    },
+    {
+        namet: 'black',
+        score: 0,
+        foul: 0,
+    },
+    {
+        namet: 'green',
+        score: 0,
+        foul: 0,
+    },
+    {
+        namet: 'grey',
+        score: 0,
+        foul: 0,
+    },
+]
+
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+for (let team of teamList) { 
+  team.score = randomNumber(1,10);
+  team.foul = randomNumber(1,10);
+};
+
+console.log(teamList);
